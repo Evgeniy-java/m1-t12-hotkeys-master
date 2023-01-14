d5df0b3ef3bd92e0bdc0f4d4f0d31b65ff91210f
Evgeniy-java <89537796626@mail.ru>
2023-01-14 14:53:55 +0700

    Сделал парвки по заданию: В файле класса Ai95Calculator переименуйте переменную price в ai95Price. В файле класса Ai92Calculator переименуйте переменную price в  ai92Price. Переименуйте класс PetrolCalculatorClasses в PetrolCalculator. Переименуйте метод calculate() класса PetrolCalculator в calculateLoop(). Преобразуйте строчку System.out.println("Введенные аргументы:" + args); в многострочный комментарий.

--- src/PetrolCalculator.java
+++ src/PetrolCalculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class PetrolCalculatorClasses {
-    public void calculate() {
+public class PetrolCalculator {
+    public void calculateLoop() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите количество действий => ");
         int iterationsCount = scanner.nextInt();
@@ -11,17 +11,19 @@ public class PetrolCalculatorClasses {
             System.out.print("Введите количество литров => ");
             double razmer = scanner.nextDouble();
             if (petrolType == 1) {
-                double price = new Ai92Calculator().calculate(razmer);
-                System.out.println("Стоимость " + razmer + " литров АИ-92 = " + price + "₽");
+                double ai92Price = new Ai92Calculator().calculate(razmer);
+                System.out.println("Стоимость " + razmer + " литров АИ-92 = " + ai92Price + "₽");
             } else if (petrolType == 2) {
-                double price = new Ai95Calculator().calculate(razmer);
-                System.out.println("Стоимость " + razmer + " литров АИ-95 = " + price + "₽");
+                double ai95Price = new Ai95Calculator().calculate(razmer);
+                System.out.println("Стоимость " + razmer + " литров АИ-95 = " + ai95Price + "₽");
             }
         }
     }
 
     public static void main(String[] args) {
+/*
 		System.out.println("Введённые аргументы:" + args);
-        new PetrolCalculatorClasses().calculate();
+*/
+        new PetrolCalculator().calculateLoop();
     }
 }
